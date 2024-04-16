c7b9fa4e38e2eb4cd23d18c5e725c9a15535e800
skwdhc <133957547+skwdhc@users.noreply.github.com>
2024-04-16 16:50:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,56 +8,56 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx6a9bbb3effcd585b',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '9b4e31ad3d7574dd4c0b947a5f319ce4',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '贵阳',
+  CITY: '乌当区',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '小露露',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oNy_A6W9srjDkCNOhzVoekRQ85YA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '5iw9LdYT3blgcoWveB9r06_o6qHYL5_ng4SInE0lQ60',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '08-09',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝贝', year: '2002', date: '08-09',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '李四', year: '1996', date: '09-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2023', date: '07-19',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2024-03-16' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2027-09-09' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'Uxfz9kFOufMrSWBLi54BHGrmDuZ_nJ4bMgOvh5IUDO0',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '小凯',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oNy_A6VdcMytJ-pLH7ubP4rhbgtg',
     }
   ],
 
